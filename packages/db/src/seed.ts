4671e6ed529e3db84b27dbd1851c12fc01293070
Albin Carlsson <github.garter593@passmail.net>
2025-11-19 15:00:06 +0100

    seed

---
+++ packages/db/src/seed.ts
@@ -0,0 +1,19 @@
+import { DB_SCHEMA } from '.';
+import { channelIds } from './channeIds';
+import { getDbConnection } from './connection';
+
+async function main() {
+	const dbClient = getDbConnection(Bun.env.DATABASE_URL!);
+
+	await dbClient.insert(DB_SCHEMA.channels).values(
+		channelIds.map((channel) => ({
+			ytChannelId: channel.id,
+			name: channel.name
+		}))
+	);
+	console.log('Channels inserted successfully');
+
+	await dbClient.$client.end();
+}
+
+main();
