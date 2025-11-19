aa67a41b89cedf5c8b54c98c18fb6c51b147e836
Albin Carlsson <github.garter593@passmail.net>
2025-11-19 12:42:52 +0100

    rm redis

--- apps/bun-worker/src/db/index.ts
+++ apps/bun-worker/src/db/index.ts
@@ -1,9 +1,9 @@
 import { getDbConnection } from '@hc/db';
-import { RedisClient } from 'bun';
+// import { RedisClient } from 'bun';
 
 export const dbClient = getDbConnection(Bun.env.DATABASE_URL!);
 
-export const redisDbClient = new RedisClient(Bun.env.REDIS_URL!);
+// export const redisDbClient = new RedisClient(Bun.env.REDIS_URL!);
 
 export * from './queries';
 export * from './mutations';
