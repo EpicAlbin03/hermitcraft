c46f11f842531c74922375325155330e217e8b41
Albin Carlsson <github.garter593@passmail.net>
2025-11-19 13:25:04 +0100

    channeIds

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,3 +1,23 @@
+import { DB_SCHEMA } from '.';
+import { channelIds } from './channeIds';
+import { getDbConnection } from './connection';
+
 export * as DB_SCHEMA from './schema';
 export * from './connection';
 export * from 'drizzle-orm';
+
+async function main() {
+	const dbClient = getDbConnection(Bun.env.DATABASE_URL!);
+
+	await dbClient.insert(DB_SCHEMA.channels).values(
+		channelIds.map((channel) => ({
+			ytChannelId: channel.id,
+			name: channel.name
+		}))
+	);
+	console.log('Channels inserted successfully');
+
+	await dbClient.$client.end();
+}
+
+main();
