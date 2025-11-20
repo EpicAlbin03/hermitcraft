b65785723a12f99c5dd76a14ebeac002bf0f5c49
Albin Carlsson <github.garter593@passmail.net>
2025-11-20 16:20:04 +0100

    toggle mode

--- apps/sv-web/src/routes/(app)/+layout.svelte
+++ apps/sv-web/src/routes/(app)/+layout.svelte
@@ -4,6 +4,7 @@
 	import AppSidebar from '$lib/components/app-sidebar.svelte';
 	import { Separator } from '$lib/components/ui/separator/index.js';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import ToggleMode from '$lib/components/toggle-mode.svelte';
 
 	let { children } = $props();
 
@@ -14,11 +15,14 @@
 	<AppSidebar {channels} />
 	<Sidebar.Inset>
 		<header
-			class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12"
+			class="flex h-16 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12"
 		>
 			<div class="flex items-center gap-2 px-4">
 				<Sidebar.Trigger class="-ml-1" />
 				<Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
+			</div>
+			<div class="flex items-center gap-2 px-4">
+				<ToggleMode />
 			</div>
 		</header>
 		<div class="flex flex-1 flex-col gap-4 p-4 pt-0">
