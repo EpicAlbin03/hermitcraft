1388017451a9f866fe89db0d05830cd12a640195
Albin Carlsson <89704855+EpicAlbin03@users.noreply.github.com>
2026-05-16 18:01:26 +0200

    rm dotenv

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,10 +1,9 @@
-import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
 	dialect: 'postgresql',
 	schema: './src/schema.ts',
 	dbCredentials: {
-		url: process.env.DATABASE_URL!
+		url: Bun.env.DATABASE_URL!
 	}
 });
