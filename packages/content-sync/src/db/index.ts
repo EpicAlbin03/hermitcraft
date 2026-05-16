2ec2993cfd92fe14744f1567621a829d392fca11
Albin Carlsson <89704855+EpicAlbin03@users.noreply.github.com>
2026-05-16 20:19:02 +0200

    getChannel

--- packages/content-sync/src/db/index.ts
+++ packages/content-sync/src/db/index.ts
@@ -1,8 +1,10 @@
 import { DB } from '@hc/db/connection';
 import { DB_SCHEMA, type Channel } from '@hc/db/schema';
-import { Context, Effect } from 'effect';
+import * as Option from 'effect/Option';
+import * as Effect from 'effect/Effect';
+import * as Context from 'effect/Context';
 import * as Data from 'effect/Data';
-import { getColumns } from 'drizzle-orm';
+import { eq, getColumns } from 'drizzle-orm';
 
 class DbError extends Data.TaggedError('DbError')<{ message: string; cause?: unknown }> { }
 
@@ -29,12 +31,33 @@ const dbService = Effect.gen(function* () {
 				)
 			);
 
+	const getChannel = (ytChannelId: string): Effect.Effect<Option.Option<Channel>, DbError> =>
+		db
+			.select(channelColumns)
+			.from(DB_SCHEMA.channels)
+			.where(eq(DB_SCHEMA.channels.ytChannelId, ytChannelId))
+			.limit(1)
+			.pipe(
+				Effect.map(([channel]) => Option.fromNullishOr(channel)),
+				Effect.mapError(
+					(cause) =>
+						new DbError({
+							message: 'Failed to get channel',
+							cause
+						})
+				)
+			);
+
 	return {
-		getAllChannels
+		getAllChannels,
+		getChannel
 	};
 });
 
-type DbServiceShape = Effect.Success<typeof dbService>;
+
+
+
+type DbServiceShape = Effect.Success<typeof dbService >    ;
 
 export class DbService extends Context.Service<DbService, DbServiceShape>()(
 	"@hc/content-sync/db/DbService"
