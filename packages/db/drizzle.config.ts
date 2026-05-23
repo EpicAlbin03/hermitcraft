d3e251196b1bd9e89d6737577330c9c91ead9e84
Albin Carlsson <89704855+EpicAlbin03@users.noreply.github.com>
2026-05-23 15:37:51 +0200

    rm migrations

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,9 +1,16 @@
+import process from 'node:process';
 import { defineConfig } from 'drizzle-kit';
+
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+	throw new Error('DATABASE_URL is required');
+}
 
 export default defineConfig({
 	dialect: 'postgresql',
 	schema: './src/schema.ts',
 	dbCredentials: {
-		url: Bun.env.DATABASE_URL!
+		url: databaseUrl
 	}
 });
