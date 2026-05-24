273e5c692dbd565e36b7e45ddbbe988b3e0ee378
Albin Carlsson <89704855+EpicAlbin03@users.noreply.github.com>
2026-05-24 18:04:34 +0200

    assets

---
+++ apps/web/src/lib/assets/svg/youtube.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import type { SVGAttributes } from 'svelte/elements';
+
+	let props: SVGAttributes<SVGElement> = $props();
+</script>
+
+<!-- <?xml version="1.0" encoding="utf-8"?> -->
+<!-- Generator: Adobe Adobe Illustrator 29.2.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<svg
+	version="1.1"
+	id="Layer_1"
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:xlink="http://www.w3.org/1999/xlink"
+	x="0px"
+	y="0px"
+	width="602.19px"
+	height="515.87px"
+	viewBox="0 0 602.19 515.87"
+	style="enable-background:new 0 0 602.19 515.87;"
+	xml:space="preserve"
+	class="size-5!"
+	{...props}
+>
+	<style type="text/css">
+		.st0 {
+			fill: #ff0033;
+		}
+		.st1 {
+			fill: #ffffff;
+		}
+	</style>
+	<g>
+		<path
+			class="st0"
+			d="M146.28,388.42c-17.09-4.63-30.41-18.25-35.34-35.05c-8.11-30.99-8.11-95.59-8.11-95.59s0-64.59,8.11-95.29
+		c4.92-17.09,18.25-30.41,35.34-35.05c30.7-8.4,154.67-8.4,154.67-8.4s124.26,0,154.96,8.4c17.38,4.63,30.41,17.96,35.05,35.05
+		c8.4,30.7,8.4,95.29,8.4,95.29s0,64.59-8.4,95.59c-4.64,16.8-17.67,30.41-35.05,35.05c-30.7,8.4-154.96,8.4-154.96,8.4
+		S176.98,396.82,146.28,388.42z M261.56,317.46l102.83-59.67l-102.83-59.38V317.46z"
+		/>
+		<polygon class="st1" points="261.56,317.46 364.38,257.79 261.56,198.41 	" />
+	</g>
+</svg>
