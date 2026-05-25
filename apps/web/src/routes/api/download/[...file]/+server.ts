5236c2bb3f80d9babf4635585df4e556e5199396
Albin Carlsson <89704855+EpicAlbin03@users.noreply.github.com>
2026-05-25 20:35:53 +0200

    s3 api

---
+++ apps/web/src/routes/api/download/[...file]/+server.ts
@@ -0,0 +1,16 @@
+import { s3 } from 'bun';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async ({ params }) => {
+	const file = s3.file(params.file);
+
+	return new Response(file.stream(), {
+		status: 200,
+		headers: {
+			'Content-Type': 'application/octet-stream', // generic binary
+			'Content-Disposition': `attachment; filename="${params.file}"`, // triggers download
+			'Cache-Control': 'public, max-age=86400, immutable',
+			'Content-Length': file.size.toString()
+		}
+	});
+};
