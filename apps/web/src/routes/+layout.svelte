e77bbd8e9ccc56313c4e2e25b299ce7d67306f06
Albin Carlsson <89704855+EpicAlbin03@users.noreply.github.com>
2026-05-25 20:17:35 +0200

    layout

---
+++ apps/web/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import TailwindIndicator from '$lib/components/tailwind-indicator.svelte';
+	import { UserConfig, UserConfigContext } from '$lib/config/user-config.svelte';
+	import type { LayoutProps } from './$types';
+	import './layout.css';
+	import { ModeWatcher } from 'mode-watcher';
+
+	let { children, data }: LayoutProps = $props();
+
+	// svelte-ignore state_referenced_locally
+	UserConfigContext.set(new UserConfig(data.userConfig));
+</script>
+
+<ModeWatcher />
+<TailwindIndicator />
+{@render children()}
