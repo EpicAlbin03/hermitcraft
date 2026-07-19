bd1912c018e8f28e2b011530600a1691ce42f697
Albin Carlsson <89704855+EpicAlbin03@users.noreply.github.com>
2026-07-19 11:56:48 +0200

    content-sync init

---
+++ packages/content-sync/.prettierrc.ts
@@ -0,0 +1,8 @@
+import { type Config } from "prettier"
+import baseConfig from "@hc/prettier-config/base"
+
+const config: Config = {
+	...baseConfig
+}
+
+export default config
---
+++ packages/content-sync/eslint.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "eslint/config"
+import baseConfig from "@hc/eslint-config/base"
+
+export default defineConfig(
+	baseConfig,
+	{
+		languageOptions: {
+			parserOptions: {
+				tsconfigRootDir: import.meta.dirname
+			}
+		}
+	},
+	{
+		ignores: ["node_modules", "build", ".turbo"]
+	}
+)
